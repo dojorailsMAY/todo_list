ae0f1718451f6cfdb024c345050a20697aad951e
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-05-16 15:26:40 -0500

    removed the wall

--- the_wall/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-    has_many :comments
-    has_many :posts
-    validates :first_name, :last_name, :email, :password, :presence => true, :length => {:minimum => 2}
-    validates :email, uniqueness: true
-end
